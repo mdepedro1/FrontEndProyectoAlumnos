c1c10fd23c84ae06fb8561136dc5e2b5c7af8a8f
mdpedro1 <mdepedro@hiberus.com>
2025-09-26 12:59:00 +0200

    nuevo commit

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import WelcomeView from './components/WelcomeView.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <WelcomeView msg="¡Bienvenido al gestor de alumnos en prácticas de hiberus!" />
+      <WelcomeView msg="¡Bienvenido al gestor de alumnos en prácticas de hiberus!aaaa" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
